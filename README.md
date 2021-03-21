a435287435c3623faa7b57a098323b29a073dc02
a.gareev <a.gareev@city-mobil.ru>
2021-03-21 22:48:43 +0300

    add analyzer.go

---
+++ README.md
@@ -0,0 +1,9 @@
+# zlogkeyscheck
+
+zlogkeyscheck is a go static analysis tool to find rs/zerolog log keys types mismatches
+
+Example of log key mismatch:
+```go
+    log.Info().Str("device_id", "1").Msg("device_id passed as string")
+    log.Info().Int("device_id", 1).Msg("device_id passed as int")
+```
---
+++ pkg/analyzer/analyzer.go
@@ -0,0 +1,73 @@
+package analyzer
+
+import (
+	"go/ast"
+	"golang.org/x/tools/go/analysis"
+)
+
+var logTypes = []string{
+	"Str", "Strs", "Bytes", "Hex", "Errs", "Err", "Bool", "Bools",
+	"Int", "Ints", "Int8", "Ints8", "Int16", "Ints16", "Int32", "Ints32", "Int64", "Ints64",
+	"Uint", "Uints", "Uint8", "Uints8", "Uint16", "Uints16", "Uint32", "Uints32", "Uint64", "Uints64",
+	"Time", "Times", "Dur", "Durs",
+}
+
+var logFieldMap = map[string]string{}
+
+var Analyzer = &analysis.Analyzer{
+	Name: "zlogkeyscheck",
+	Doc:  "Checks rs/zerolog log keys types mismatches",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := func(node ast.Node) bool {
+		funcDecl, ok := node.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+
+		fun, ok := funcDecl.Fun.(*ast.SelectorExpr)
+		if !ok {
+			return true
+		}
+		//|| fun.Sel.Name != "Int64"
+		if !inArrayString(fun.Sel.Name, logTypes) {
+			return true
+		}
+
+		if len(funcDecl.Args) < 1 {
+			return true
+		}
+		arg, ok := funcDecl.Args[0].(*ast.BasicLit)
+		if !ok {
+			return true
+		}
+
+		if prevType, ok := logFieldMap[arg.Value]; ok {
+			if prevType != fun.Sel.Name {
+				pass.Reportf(node.Pos(), "Bad %s log key type of \"%s\" previously was used as \"%s\"\n",
+					arg.Value, fun.Sel.Name, prevType)
+				return true
+			}
+		} else {
+			logFieldMap[arg.Value] = fun.Sel.Name
+		}
+
+		return true
+	}
+
+	for _, f := range pass.Files {
+		ast.Inspect(f, inspect)
+	}
+	return nil, nil
+}
+
+func inArrayString(value string, haystack []string) bool {
+	for _, v := range haystack {
+		if value == v {
+			return true
+		}
+	}
+	return false
+}
